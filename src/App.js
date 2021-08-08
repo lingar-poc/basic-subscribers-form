c6fe4f30ccda2b68630e5e46a90ab0c1cd6920d4
lingar <yimprogramming@gmail.com>
2021-08-08 17:35:51 +0300

    Adding some text

--- src/App.js
+++ src/App.js
@@ -6,11 +6,46 @@ import {SubscribersForm} from "./subscriber-form/SubscribersForm";
  */
 
 function App() {
-  return (
-    <div className="App">
-      <SubscribersForm/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <br/>
+            <SubscribersForm/>
+            <br/>
+            <br/>
+            <br/>
+            <p>
+                Here is a form which make validation on all fields (if they are exist), and also do validation if the
+                email is valid.<br/>
+                The email validation happens by delay (for not check in every type) by using a custom hook.
+            </p>
+            <hr/>
+
+            <div className= "bottom-area">
+                <h4>
+                    Done by lingar (Izhar Mashkif), YIM - Programming
+
+                </h4>
+                <h5>
+                    Creating large web systems from the scratch.
+                    <br/>
+                    Java, React, Angular, DB’s, Deployment, Clouds and more.
+                    <br/>
+                    Per Hour/ Per Project.
+                    <br/>
+                    Available worldwide for remote work, and for on-site work in Israel in the central and Jerusalem areas
+
+
+                </h5>
+                <h1> “DO IT SIMPLE”.
+                </h1>
+                <a href = "https://www.linkedin.com/in/izhar-mashkif-86a01913a/">LinkedIn </a><br/>
+                <a href = "mailto:yimprogramming@gmail.com"> yimprogramming@gmail.com</a>
+
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
