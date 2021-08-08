ce8754a9db52a5080291945b869fd4535cbe30cc
lingar <yimprogramming@gmail.com>
2021-08-08 17:18:06 +0300

    Some info to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import {SubscribersForm} from "./subscriber-form/SubscribersForm";
 
+/**
+ * For deploy the app, run on the npm run deploy
+ */
+
 function App() {
   return (
     <div className="App">
